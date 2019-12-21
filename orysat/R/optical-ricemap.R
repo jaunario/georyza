23da2ad9115ec70080741e43b66c8674fe6b5374
Jorrel Khalil S. Aunario <jaunario@gmail.com>
2019-12-21 11:08:03 +0800

    Ignoring RStudio files
    Excluding RStudio project files from repository
    e.g. Rproj.user, .pper, Rbuildignore Rhistory

---
+++ orysat/R/optical-ricemap.R
@@ -0,0 +1,11 @@
+# Title: Rice detection using vegetation indices from Optical Sensors
+# Description: The inputs of the functions are a time series of vegetation indices (EVI, NDVI) and water indices (NDWI)
+# Author: JAunario
+###############################################################################
+
+optical.rice <- function(index_veg, index_water, season_start=1, season_end=1){
+	
+}
+
+
+
